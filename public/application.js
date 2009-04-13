b937c2d499c5320dca5048dd967e600582ab6172
Nick Plante <nap@zerosum.org>
2009-04-13 17:23:38 -0400

    style new project form

--- public/application.js
+++ public/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('li.project, .button a').hover(
+  $('li.project, .button a, :submit').hover(
     function() { $(this).addClass('highlight'); },
     function() { $(this).removeClass('highlight'); }
   );
