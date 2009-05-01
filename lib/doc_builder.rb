a101508519e9cc67d9f6c56d9c38b6738a556337
Jeff Rafter <jeff@baobabhealth.org>
2009-05-01 16:03:26 -0700

    use the deploy user instead

--- lib/doc_builder.rb
+++ lib/doc_builder.rb
@@ -87,16 +87,16 @@ class DocBuilder
   def init_pages
     FileUtils::mkdir_p SiteConfig.rdoc_dir
     Dir.chdir(SiteConfig.rdoc_dir)
-    return unless `sudo su - docs -c 'cd #{SiteConfig.rdoc_dir}; git status'` =~ /Not a git repository/
-    `sudo su - docs -c 'cd #{SiteConfig.rdoc_dir}; git init'`
-    `sudo su - docs -c 'cd #{SiteConfig.rdoc_dir}; git remote add origin git@github.com:docs/docs.github.com.git'`
-    `sudo su - docs -c 'cd #{SiteConfig.rdoc_dir}; git pull origin master'`
+    return unless `git status` =~ /Not a git repository/
+    `git init`
+    `git remote add origin git@github.com:docs/docs.github.com.git`
+    `git pull origin master`
   end
   
   def push_pages
     Dir.chdir(SiteConfig.rdoc_dir)
-    `sudo su - docs -c 'cd #{SiteConfig.rdoc_dir}; git add .'`
-    `sudo su - docs -c 'cd #{SiteConfig.rdoc_dir}; git commit -a -m "Updating documentation for #{@project.owner}/#{@project.name}"'`
-    `sudo su - docs -c 'cd #{SiteConfig.rdoc_dir}; git push origin master'`
+    `git add .`
+    `git commit -a -m "Updating documentation for #{@project.owner}/#{@project.name}"`
+    `git push origin master`
   end
 end
