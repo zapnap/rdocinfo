1003b324d0b8d27e7ff746be11acf011f1ea65a7
Jeff Rafter <jeff@baobabhealth.org>
2009-05-07 10:48:08 -0700

    Be mean to yard. Change it from the template

--- templates/github/helpers.rb
+++ templates/github/helpers.rb
@@ -5,3 +5,19 @@ $github[:branch] = ENV['GH_BRANCH']
 $github[:version] = ENV['GH_VERSION']
 $github[:description] = ENV['GH_DESCRIPTION']
 
+# This is just not cool
+module YARD
+  module Generators
+    class FullDocGenerator < Base
+      def generate_index
+        if format == :html && serializer
+          serializer.serialize 'index.html', render(:index)
+          serializer.serialize '/files/index.html', render(:all_files)
+          serializer.serialize '/namespaces/index.html', render(:all_namespaces)
+          serializer.serialize '/methods/index.html', render(:all_methods)
+        end
+        true
+      end
+    end
+  end    
+end
